8c4eb7d3c44ec7303e6963f033d9414d2663f7ab
Gio- <saffirio.giorgio@gmail.com>
2017-01-26 11:30:45 +0100

    Grid Manager Added

---
+++ p_bit/Assets/Scripts/GridManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridManager : MonoBehaviour {
+
+	public int col;
+	public int row;
+	public GameObject tile;
+
+	private char[,] grid;
+
+	// Use this for initialization
+	void Start(){
+		grid = new char[col, row];
+		GenerateGrid ();
+	}
+
+	private void GenerateGrid () {
+		for (int i = 0; i < col; i++) {
+			for (int j = 0; j < row; j++) {
+				Instantiate (tile, new Vector3 (transform.position.x + i, transform.position.y, transform.position.z + j), Quaternion.identity);
+				grid [i, j] = 'X';
+			}
+		}
+	}
+
+	public bool checkEnemyInPosition(int x, int y){
+		if (grid [x, y] != 'X')
+			return true;
+		return false;
+	}
+}
